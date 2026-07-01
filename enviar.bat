3e16cee3bb91c520528a4a3087550f0bb23e29a4
Robson Farias Barros Farias <robinrfb49@gmail.com>
2026-07-01 08:51:30 -0300

    Atualizacao automatica: 01/07/2026  8:51:30,09

---
+++ enviar.bat
@@ -0,0 +1,10 @@
+@echo off
+cd C:\projetoauditoria
+echo Adicionando alteracoes...
+git add .
+echo Salvando alteracoes...
+git commit -m "Atualizacao automatica: %date% %time%"
+echo Enviando para a nuvem...
+git push origin master
+echo Pronto! Pagina atualizada.
+pause
